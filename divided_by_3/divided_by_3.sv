e3acf0faa4215c011f656584784673b5f539d17a
venu <venukumar71022@gmail.com>
2026-01-12 00:16:31 +0530

    code is udpated

--- divided_by_3/divided_by_3.sv
+++ divided_by_3/divided_by_3.sv
@@ -1,12 +1,15 @@
 module divided_by_3_tb();
    logic   i_clk;
    logic   i_rst;
-   logic   mod0= 0;
-   logic   mod1= 1;
-   logic   mod2= 2;
-   logic   mod3= 3;
-   logic   modr= 0;
-   logic   state;
+   typedef enum logic [2:0] {
+      mod0= 3'h1,
+      mod1= 3'h2,
+      mod2= 3'h3,
+      mod3= 3'h4,
+      modr= 3'h0 } state_t;
+   state_t state;
+   logic   in;
+   logic   out;
 
    initial begin
       i_clk = 0;
@@ -25,9 +28,19 @@ module divided_by_3_tb();
    always_ff @(posedge i_clk) begin
       if(i_rst == 1) begin
          state <= modr;
+         in    <= 0;
       end else begin
-         
+         in <= $urandom_range(0, 1);
+         case(state)
+            modr : state <= in ? mod1 : mod0;
+            mod0 : state <= in ? mod1 : mod0;
+            mod1 : state <= in ? mod3 : mod2;
+            mod2 : state <= in ? mod2 : mod1;
+            mod3 : state <= in ? mod1 : mod0;
+         endcase
       end 
    end 
+   
+   assign out = state == mod3  ? 1 : 0;
       
 endmodule
